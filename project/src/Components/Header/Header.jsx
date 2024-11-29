fb00fa19795790b56840269a58b4c46cd84b9634
Ivan_Hlibov <92669197+Ivanhlibov@users.noreply.github.com>
2024-11-30 00:08:18 +0200

    add window

--- project/src/Components/Header/Header.jsx
+++ project/src/Components/Header/Header.jsx
@@ -1,107 +1,128 @@
-import React from 'react'
-import Tea from '../../Images/Tea.svg'
-import Buy from '../../Images/Buy.svg'
-import User from '../../Images/User.svg'
-import Search from '../../Images/Search.svg'
-import Close from '../../Images/close.svg'
+import React, { useEffect } from 'react';
+import Tea from '../../Images/Tea.svg';
+import Buy from '../../Images/Buy.svg';
+import User from '../../Images/User.svg';
+import Search from '../../Images/Search.svg';
+import Close from '../../Images/close.svg';
 
-import './Header.css'
-import '../../index.css'
+import './Header.css';
+import '../../index.css';
 
 const Header = () => {
+  useEffect(() => {
+    const menuBtn = document.querySelector('.menu__btn');
+    const menuClose = document.querySelector('.menu_close-btn');
+    const menuList = document.querySelector('.menu__list');
+
+    if (menuBtn && menuList) {
+      menuBtn.addEventListener('click', () => {
+        menuList.classList.add('menu__list--open');
+      });
+    }
+
+    if (menuClose && menuList) {
+      menuClose.addEventListener('click', () => {
+        menuList.classList.remove('menu__list--open');
+      });
+    }
+
+    return () => {
+      // Очищаем обработчики событий при размонтировании компонента
+      if (menuBtn) menuBtn.removeEventListener('click', () => {});
+      if (menuClose) menuClose.removeEventListener('click', () => {});
+    };
+  }, []);
+
   return (
     <div>
       <header className="header">
         <div className="container">
-            <div className="header__inner">
-                <a href="#" className="logo">
-                    <img src={Tea} alt="" />
+          <div className="header__inner">
+            <a href="#" className="logo">
+              <img src={Tea} alt="Tea Logo" />
+            </a>
+            <nav className="menu">
+              <button className="menu__btn">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </button>
+              <ul className="menu__list">
+                <li className="menu_close">
+                  <button className="menu_close-btn">
+                    <img src={Close} alt="Close Menu" />
+                  </button>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">Black</a>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">Green</a>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">White</a>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">Oolong</a>
+                </li>
+                <li className="menu__item">
+                  <a href="#" className="menu__link">Pu-erh</a>
+                </li>
+                <li className="mobile-nav">
+                  <ul className="mobile_menu">
+                    <li className="mobile-menu__item">
+                      <a href="#" className="mobile__menu-link">Contact us</a>
+                    </li>
+                    <li className="mobile-menu__item">
+                      <a href="#" className="mobile__menu-link">Our blog</a>
+                    </li>
+                    <li className="mobile-menu__item">
+                      <a href="#" className="mobile__menu-link">Shipping and Delivery</a>
+                    </li>
+                  </ul>
+                  <ul className="mobile-actions">
+                    <li className="mobile-actions__item">
+                      <a href="#" className="user-actions__link">
+                        <img src={Buy} alt="Buy" />
+                      </a>
+                    </li>
+                    <li className="mobile-actions__item">
+                      <a href="#" className="user-actions__link">
+                        <img src={User} alt="User" />
+                      </a>
+                    </li>
+                    <li className="mobile-actions__item">
+                      <a href="#" className="user-actions__link">
+                        <img src={Search} alt="Search" />
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+            <ul className="user-actions">
+              <li className="user-actions__item">
+                <a href="#" className="user-actions__link">
+                  <img src={Buy} alt="Buy" />
                 </a>
-                <nav className="menu">
-                    <button className="menu__btn">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </button>
-                    <ul className='menu__list' >
-                        <li className="menu_close">
-                       <button className='menu_close-btn' >
-                       <img src={Close} alt="" />
-                       </button>
-                        </li>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link">Black</a>
-                            </li>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link">Green</a>
-                        </li>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link">White</a>
-                        </li>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link">Oolong</a>
-                        </li>
-                        <li className="menu__item">
-                            <a href="#" className="menu__link">Pu-erh</a>
-                        </li>
-
-                        <li className='mobile-nav ' >
-                            <ul className='mobile_menu' >
-                               <li className="mobile-menu__item">
-                                <a href="#" className="mobile__menu-link">Contct us</a>
-                                </li>
-                               <li className="mobile-menu__item">
-                                <a href="#" className="mobile__menu-link">Our blog</a>
-                                </li>
-                               <li className="mobile-menu__item">
-                                <a href="#" className="mobile__menu-link">Shipping and Delivery</a>
-                                </li>
-                            </ul>
-                            <ul className="mobile-actions">
-                            <li className="mobile-actions__item">
-                                <a href="#" className="user-actions__link">
-                            <img src={Buy} alt="" /></a>
-                            </li>
-                            <li className="mobile-actions__item">
-                              <a href="#" className="user-actions__link">
-                                 <img src={User} alt="" /></a>
-                            </li>
-                            <li className="mobile-actions__item ">
-                              <a href="#" className="user-actions__link">
-                                <img src={Search} alt="" /></a>
-                            </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-                <ul className="user-actions">
-                    <li className="user-actions__item">
-                        <a href="#" className="user-actions__link">
-                            <img src={Buy} alt="" /></a>
-                    </li>
-                    <li className="user-actions__item">
-                        <a href="#" className="user-actions__link">
-                            <img src={User} alt="" /></a>
-                    </li>
-                    <li className="user-actions__item user-actions__item-search ">
-                        <a href="#" className="user-actions__link">
-                            <img src={Search} alt="" /></a>
-                    </li>
-                    <li className="user-actions__item">
-                        {/* <search>
-                            <form action='' >
-                               <input type='search' ></input>
-                               <button type='submit' ></button>
-                            </form>
-                        </search> */}
-                    </li>
-                </ul>
-            </div>
+              </li>
+              <li className="user-actions__item">
+                <a href="#" className="user-actions__link">
+                  <img src={User} alt="User" />
+                </a>
+              </li>
+              <li className="user-actions__item user-actions__item-search">
+                <a href="#" className="user-actions__link">
+                  <img src={Search} alt="Search" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
